2db5468ea7c3a4c40f55b97fb7ac6c343ce9aa9d
Jo Yun Ha <ehdhbd68@naver.com>
2024-11-30 17:27:58 +0900

    h

---
+++ src/step/step04.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Step04 = () => {
+
+    /* 
+        [ 제네릭 ]
+        타입을 매개변수로 받아 사용하는 방법
+        제네릭을 사용하면 코드의 재사용성을 높이고
+        다양한 타입에 대해 유연하게 동작하는 코드를 작성가능 
+    */
+
+   function identity<T>(value : T) : T{
+     return value;
+   }
+
+   const numberValue = identity<number>(10);
+   const strValue = identity<string>('hi');
+
+  return (
+    <div>Step04</div>
+  )
+}
+
+export default Step04
